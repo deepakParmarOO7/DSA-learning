fe7e4262d89d131ceb27193a9d52773bd20c20d1
Deepak Parmar <deepak.parmar@catalystone.com>
2025-06-13 11:13:23 +0530

    fix: insertion and selection sort

---
+++ Sorting/insertionSort.js
@@ -0,0 +1,4 @@
+// Insertion sort is stable alogorithm
+// Time complexity: 
+// Best case: Linear time(sorted array)
+// In general o(n)sq, worst case: Reverse sorted
--- Sorting/selectionSort.js
+++ Sorting/selectionSort.js
@@ -1,2 +1,26 @@
 // less memory writes compared to quicksort, mergesort, Insertion sort. But 
 // cycle sort is optimal in terms of memory writes.
+
+const selectionSort = (arr) =>{
+    var minElement = arr[0]
+    var minIndex = 0
+
+    for(let i=0; i<arr.length; i++) {
+        minIndex=i
+        for(let j=i; j<arr.length; j++){
+            if(arr[j]<arr[minIndex]){
+                minIndex = j
+                console.log(minElement);
+            }
+            
+        }
+        if(minIndex!= i){
+            [arr[i],arr[minIndex]]=[arr[minIndex],arr[i]]
+        }
+    }
+    return arr
+}
+
+const arr=[2,1,5,8,7,6]
+sortedArray = selectionSort(arr)
+console.log(sortedArray);
